696a1be9691a1ec5bf696ae826032ca0ecd0fad8
Timothée Dione <timothee.dione@evooq.ch>
2025-03-28 18:11:03 +0100

    keywords

---
+++ variables.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 28 18:05:51 2025
+
+@author: timoth-e-dione
+"""
+
+a = 3
+b = 4
+B = 4
+
+A, B = 3.5, 4.5
+
+
+print(B)
+
+#mots interdits
+import keyword
+print(keyword.kwlist)
