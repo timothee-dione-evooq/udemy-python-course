3c09a59f65576b6e9e9598d3fc380718f6598788
Timothée Dione <timothee.dione@evooq.ch>
2025-04-02 17:55:54 +0200

    add ex27 and 28

---
+++ ex27-trasnform-list-into-set.py
@@ -0,0 +1,3 @@
+pays_limitrophes_france = ['Allemagne', 'Italie', 'Monaco', 'Andorre', 'Belgique', 'Luxembourg', 'Espagne', 'Suisse']
+dico = {i: value for i, value in enumerate(pays_limitrophes_france, start=0)}
+print(dico)
---
+++ ex28-transform-list-into-tuples-list.py
@@ -0,0 +1,3 @@
+pays_limitrophes_france = ['Allemagne', 'Italie', 'Monaco', 'Andorre', 'Belgique', 'Luxembourg', 'Espagne', 'Suisse']
+tuples = [(i, value) for i, value in enumerate(pays_limitrophes_france)]
+print(tuples)
